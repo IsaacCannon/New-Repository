8580f53ced345ed67c20c0fc743695fef48d55be
IsaacCannon <54118713+IsaacCannon@users.noreply.github.com>
2022-06-25 13:37:14 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" << endl;
+    cout << "Whats up" << endl;
+    cout << "thats awesome" << endl;
+
+    return 0;
+}
